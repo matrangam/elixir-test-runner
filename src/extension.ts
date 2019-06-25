c21b83ef60880d34d56fc40602d38a6e667b8391
Michael Matranga <m.matranga@printfly.com>
2019-06-25 09:34:20 -0400

    Initial commit.

---
+++ src/extension.ts
@@ -0,0 +1,86 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vscode = require("vscode");
+
+function activate(context) {
+
+	const command = 'exlixir_test_runner.run_test';
+	const runTest = (args) => {
+		const index = args.uri.indexOf("test")
+		const actualURI = args.uri.slice(index)
+		const command = `./scripts/test ${actualURI}:${args.lineNumber}`
+		console.log(command);
+
+		const terminal = vscode.window.createTerminal();
+		terminal.show();
+		terminal.sendText(command)
+	};
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand(command, runTest)
+	);
+
+	let timeout = undefined;
+	const testDecorationType = vscode.window.createTextEditorDecorationType({
+		cursor: 'crosshair',
+		backgroundColor: { id: 'myextension.largeNumberBackground' }
+	});
+	let activeEditor = vscode.window.activeTextEditor;
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+		}
+		const regEx = /(test|describe) ".+"/g
+		const text = activeEditor.document.getText();
+		const testMatches = [];
+		let match;
+		while (match = regEx.exec(text)) {
+			const startPos = activeEditor.document.positionAt(match.index);
+			const endPos = activeEditor.document.positionAt(
+				match.index + match[0].length
+			);
+
+			const args = {
+				uri: activeEditor.document.uri.path,
+				lineNumber: startPos.line + 1
+			};
+			const hoverMessage = new vscode.MarkdownString(
+				`[Run Suite: ${
+				match[0]
+				}](command:exlixir_test_runner.run_test?${encodeURIComponent(
+					JSON.stringify(args)
+				)})`
+			);
+			hoverMessage.isTrusted = true;
+			const decoration = {
+				range: new vscode.Range(startPos, endPos),
+				hoverMessage: hoverMessage
+			};
+			testMatches.push(decoration);
+		}
+		activeEditor.setDecorations(testDecorationType, testMatches);
+	}
+	function triggerUpdateDecorations() {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+		timeout = setTimeout(updateDecorations, 500);
+	}
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+}
+exports.activate = activate;
+//# sourceMappingURL=extension.js.map
